6de1e3c665a2ad5a3c2bce2531a30ddf40a7df52
Alfredo <atakori@outlook.com>
2017-10-18 19:47:09 -0400

    added first test and added heroku

--- test/test-server.js
+++ test/test-server.js
@@ -17,4 +17,21 @@ describe('Blog Posts', function() {
     return closeServer();
   });
 
+  //GET test strategy
+  // make a GET request to /blog-posts
+  // inspect object to make sure it has keys in res obj
+  it('returns a list of post information with a GET request', function() {
+  	return chai.request(app)
+  	.get('/blog-posts')
+  	.then(function(res) {
+  		res.should.have.status(200);
+  		res.should.be.json;
+  		res.body.should.be.a('array');
+  		const expectedKeys = ['title', 'content', 'author', 'publishDate'];
+  		res.body.forEach(function(item) {
+  			item.should.include.keys(expectedKeys);
+  			item.should.be.a('object');
+  			});
+  		});
+  	});
 })
