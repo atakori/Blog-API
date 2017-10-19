54da409ae29bfdad475b17a5a457b0ad9caf3c79
Alfredo <atakori@outlook.com>
2017-10-18 22:18:14 -0400

    added POST request not working

--- test/test-server.js
+++ test/test-server.js
@@ -34,4 +34,23 @@ describe('Blog Posts', function() {
   			});
   		});
   	});
-})
+
+  //POST Request
+  //make new object to be added to the list of blogs
+  //check res obj to see if the return obj has an ID
+  it('Make a new POST request', function () {
+    const newPost = {title: 'Info', content: 'random info here', author: 'Alfredo T.', publishDate: 'Today!'};
+    return chai.request(app)
+    .post('/blog-posts')
+    .send(newPost)
+    .then(function(res) {
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.include.keys('title', 'content', 'author', 'publishDate', 'id');
+      res.body.id.should.not.be.null;
+
+      res.body.should.deep.equal(Object.assign(newPost, {id: res.body.id}));
+    });
+  });
+});
